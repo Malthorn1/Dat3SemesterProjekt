59c5c9379cecf6e78a7c8788d8bae19284af08ad
Malthorn1 <mikkelskott@gmail.com>
2020-04-30 10:59:37 +0200

    covid data

--- src/main/java/dtos/CovidDTO.java
+++ src/main/java/dtos/CovidDTO.java
@@ -5,10 +5,93 @@
  */
 package dtos;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.lang.reflect.Array;
+
+
 /**
  *
  * @author casper
  */
 public class CovidDTO {
+    
+    private String Country; 
+    private int Confirmed; 
+    private int Deaths; 
+    private int Recovered; 
+    private int Active; 
+    private String Date; 
+
+    public CovidDTO() {
+    }
+    
+    
+
+    public CovidDTO(JsonObject jobj) {
+        this.Country = jobj.get("Country").getAsString(); 
+        this.Confirmed =  jobj.get("Confirmed").getAsInt();
+        this.Deaths = jobj.get("Deaths").getAsInt();
+        this.Recovered = jobj.get("Recovered").getAsInt();
+        this.Active = Confirmed - Deaths - Recovered;
+        this.Date = jobj.get("Date").getAsString();
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public int getConfirmed() {
+        return Confirmed;
+    }
+
+    public void setConfirmed(int Confirmed) {
+        this.Confirmed = Confirmed;
+    }
+
+    public int getDeaths() {
+        return Deaths;
+    }
+
+    public void setDeaths(int Deaths) {
+        this.Deaths = Deaths;
+    }
+
+    public int getRecovered() {
+        return Recovered;
+    }
+
+    public void setRecovered(int Recovered) {
+        this.Recovered = Recovered;
+    }
+
+    
+
+    public int getActive() {
+        return Active;
+    }
+
+    public void setActive(int Active) {
+        this.Active = Active;
+    }
+
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+
+ 
+
+
+  
+    
     
 }
--- src/main/java/rest/NASAResource.java
+++ src/main/java/rest/NASAResource.java
@@ -2,13 +2,16 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
+import dtos.CovidDTO;
 import dtos.OpenCageDTO;
 import utils.EMF_Creator;
 import facades.NASAFacade;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import javax.json.Json;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.GET;
@@ -19,49 +22,46 @@ import javax.ws.rs.core.MediaType;
 import utils.HttpUtils;
 import utils.Settings;
 
-
 @Path("all")
 public class NASAResource {
-        private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
-                "pu",
-                "jdbc:mysql://localhost:3307/dat3",
-                "dev",
-                "ax2",
-                EMF_Creator.Strategy.CREATE);
-    private static final NASAFacade FACADE =  NASAFacade.getFacadeExample(EMF);
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
+            "pu",
+            "jdbc:mysql://localhost:3307/dat3",
+            "dev",
+            "ax2",
+            EMF_Creator.Strategy.CREATE);
+    private static final NASAFacade FACADE = NASAFacade.getFacadeExample(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-            
-    
-    
+
     @Path("info/{q}")
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public String getAllInfo(@PathParam("q") String q) throws IOException{
-        
-        
+    public String getAllInfo(@PathParam("q") String q) throws IOException {
         return getGeoInfo(q);
-        
-        
-        
-        
-        
-        
-        
-        
     }
-    
-    private static String getGeoInfo(String parameter) throws IOException{
-        String  key  = Settings.getPropertyValue("apikey.opencage");
-        String geoData = HttpUtils.fetchData("https://api.opencagedata.com/geocode/v1/json?q="+parameter+"&key="+ key);
-        JsonObject jobj  = new Gson().fromJson(geoData, JsonObject.class);
-        
-        
-        OpenCageDTO ocDTO = new  OpenCageDTO(jobj.get("results").getAsJsonArray().get(0).getAsJsonObject());
-        return  GSON.toJson(ocDTO);
-        
-    }
-    
 
 
-    
+
+    private static String getGeoInfo(String parameter) throws IOException {
+        String key = Settings.getPropertyValue("apikey.opencage");
+        String geoData = HttpUtils.fetchData("https://api.opencagedata.com/geocode/v1/json?q=" + parameter + "&key=" + key);
+        JsonObject jobj = new Gson().fromJson(geoData, JsonObject.class);
+
+        OpenCageDTO ocDTO = new OpenCageDTO(jobj.get("results").getAsJsonArray().get(0).getAsJsonObject());
+        return GSON.toJson(ocDTO);
+
+    }
+
+    private static ArrayList getCovidInfo(String parameter) throws IOException {
+        String covidInfo = HttpUtils.fetchData("https://api.covid19api.com/total/dayone/country/" + parameter);
+        JsonArray jobj = new Gson().fromJson(covidInfo, JsonArray.class);
+        ArrayList covidData = new ArrayList();
+        for (int i = 0; i < jobj.size(); i++) {
+            CovidDTO cDTO = new CovidDTO(jobj.get(i).getAsJsonObject());
+            covidData.add(cDTO);
+        }
+        return covidData;
+    }
+
 }
